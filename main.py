787fb00a3ea4f7b3ae831ceb8cb2e9a6c79919b8
Deyan Gigov <dejan.gigov2010@gmail.com>
2026-03-04 22:47:50 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask, redirect, render_template, request
+import os
+import requests
+file_path = 'templates/index.html'
+app = Flask(__name__)
+@app.route('/')
+def root():
+    return render_template('index.html')
+
+@app.route('/post')
+def create_post():
+    return render_template('post.html')
+
+@app.route('/post-data', methods=['POST'])
+def post_data():
+   data = request.form.get('link')
+   data1 = request.form.get('alt_text')
+   if not data1:
+         data1 = data
+   print(data)
+   with open(file_path, 'a') as file:
+            file.write('<li>' +"<a href='" + data + "' target='_blank'>" + data1 + '</a></li>\n')
+   return redirect("/post")
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
